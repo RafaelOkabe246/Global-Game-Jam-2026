b7f66cb7218ff5acbf491ba0ae82050c907aa4d6
RafaelOkabe246 <69397032+RafaelOkabe246@users.noreply.github.com>
2026-01-31 12:48:50 -0300

    Base do sistema de dialogos finalizada

--- Assets/Scripts/Dialogue Scripts/DialogueManager.cs
+++ Assets/Scripts/Dialogue Scripts/DialogueManager.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System.Linq;
+using System.Collections.Generic;
 
 public class DialogueManager : MonoBehaviour
 {
@@ -17,23 +18,42 @@ public class DialogueManager : MonoBehaviour
         SelectDialogue(character1);
     }
 
-    //private CharacterDialogue CheckConditionAndPriority(Character character)
-    //{
-    //    CharacterDialogue[] characterDialogues = character.characterDialogues;
+    private CharacterDialogue CheckConditionAndPriority(Character character)
+    {
+        CharacterDialogue[] characterDialogues = character.characterDialogues;
 
-    //    for (int i = 0; i < characterDialogues.Length; i++)
-    //    {
-    //        if (characterDialogues[i].dialogue.CheckIfConditionsTrue(inventoryTips.ToList()))
-    //        {
-    //            //Conditions are true
-    //        }
-    //    }
-    //}
+        List<CharacterDialogue> dialoguesConditionTrue = new List<CharacterDialogue>();
+
+        //Check conditions
+        for (int i = 0; i < characterDialogues.Length; i++)
+        {
+            if (characterDialogues[i].dialogue.CheckIfConditionsTrue(inventoryTips.ToList()))
+            {
+                //Conditions of the dialogue are true
+                dialoguesConditionTrue.Add(characterDialogues[i]);
+            }
+        }
+
+        //Check priority
+        int biggestPriorityDick = 0;
+        int biggestPriorityDickIndex = 0;
+        for (int i = 0; i < dialoguesConditionTrue.Count; i++)
+        {
+            if (dialoguesConditionTrue[i].dialogue.dialoguePriority > biggestPriorityDick)
+            {
+                biggestPriorityDickIndex = i;
+                biggestPriorityDick = dialoguesConditionTrue[i].dialogue.dialoguePriority;
+            }
+        }
+
+        CharacterDialogue returnCharacterDialogue = dialoguesConditionTrue[biggestPriorityDickIndex];
+        return returnCharacterDialogue;
+    }
 
     public void SelectDialogue(Character character)
     {
         //Check the conditions and stuff to return the dialogue
-        CharacterDialogue characterDialogue = character.characterDialogues[0];
+        CharacterDialogue characterDialogue = CheckConditionAndPriority(character);//character.characterDialogues[0];
         Dialogue dialogue = characterDialogue.dialogue;
 
         if(characterDialogue.hasPlayed == true)
