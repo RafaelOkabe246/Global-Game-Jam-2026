01b95543eda1a46c240e99a66e7eaf9aa6fd5edf
Euromir <145169459+Euromir@users.noreply.github.com>
2026-01-31 22:28:57 -0300

    Sistema de som
    sistema de som funcionado

---
+++ Assets/Scripts/Sound System/SoundSystem.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SoundSystem : MonoBehaviour
+{
+    public static SoundSystem Instance;
+
+    [Header("Audio Mixer")]
+    public AudioMixer AudioMixer;
+
+    [Header("Audio Options")]
+    [Range(0.0001f, 1f)] public float MasterVolume = 0.5f;
+    [Range(0.0001f, 1f)] public float MusicVolume = 0.5f;
+    [Range(0.0001f, 1f)] public float SFXVolume = 0.5f;
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject); 
+            return;
+        }
+
+        Instance = this;
+    }
+
+    private void Start()
+    {
+        LoadConfig();
+    }
+
+    public void SetMasterVolume(float value)
+    {
+        MasterVolume = value;
+        ApplyVolume("MasterVolume", value);
+        PlayerPrefs.SetFloat("MasterVolume", value);
+    }
+
+    public void SetMusicVolume(float value)
+    {
+        MusicVolume = value;
+        ApplyVolume("MusicVolume", value);
+        PlayerPrefs.SetFloat("MusicVolume", value);
+    }
+
+    public void SetSFXVolume(float value)
+    {
+        SFXVolume = value;
+        ApplyVolume("SFXVolume", value);
+        PlayerPrefs.SetFloat("SFXVolume", value);
+    }
+
+    public void ApplyVolume(string parameterName, float linearVolume)
+    {
+        float dB = Mathf.Log10(Mathf.Clamp(linearVolume, 0.0001f, 1f)) * 20f;
+        AudioMixer.SetFloat(parameterName, dB);
+    }
+
+    public void LoadConfig()
+    {
+        SetMasterVolume(PlayerPrefs.GetFloat("MasterVolume", 0.5f));
+        SetMusicVolume(PlayerPrefs.GetFloat("MusicVolume", 0.5f));
+        SetSFXVolume(PlayerPrefs.GetFloat("SFXVolume", 0.5f));
+    }
+}
